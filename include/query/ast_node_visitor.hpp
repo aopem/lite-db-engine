367661401b4d3d3c5620f4783dee9464952f94ed
Andrew Opem <andrewopem@gmail.com>
2023-04-03 09:11:34 -0700

    add visitor interface

---
+++ include/query/ast_node_visitor.hpp
@@ -0,0 +1,23 @@
+#ifndef __AST_NODE_VISITOR_HPP__
+#define __AST_NODE_VISITOR_HPP__
+
+#include "ast_nodes/ast_nodes.hpp"
+
+#include <string>
+
+namespace engine::query
+{
+    class AstNodeVisitor
+    {
+        public:
+            virtual ~AstNodeVisitor() = default;
+            virtual void visit(std::unique_ptr<CreateDatabaseNode> node) = 0;
+            virtual void visit(std::unique_ptr<CreateTableNode> node) = 0;
+            virtual void visit(std::unique_ptr<DeleteNode> node) = 0;
+            virtual void visit(std::unique_ptr<DropDatabaseNode> node) = 0;
+            virtual void visit(std::unique_ptr<SelectNode> node) = 0;
+            virtual void visit(std::unique_ptr<UpdateNode> node) = 0;
+    };
+};
+
+#endif // __AST_NODE_VISITOR_HPP__
